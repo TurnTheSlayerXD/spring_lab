0106e7facd89b1ebebd311d2c6b8ef7d33e19bdc
andrew <slayer2004@list.ru>
2025-12-29 21:09:49 +0300

    f

--- services/src/main/java/dto/PatchOwnerDto.java
+++
@@ -1,8 +0,0 @@
-package dto;
-
-import java.util.Date;
-
-public class PatchOwnerDto {
-    public String name;
-    public Date birthDate;
-}
--- services/src/main/java/dto/PostOwnerDto.java
+++
@@ -1,9 +0,0 @@
-package dto;
-
-import java.util.Date;
-
-public class PostOwnerDto {
-    public String name;
-    public Date birthDate;
-}
-
